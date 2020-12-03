a2bc1993f854f72f5221250d75d7899aa6111467
Viktoria Arnold <62843549+vikiarn@users.noreply.github.com>
2020-12-03 18:12:04 +0100

    Update addPercentGDP.R
    updated function

--- R/addPercentGDP.R
+++ R/addPercentGDP.R
@@ -7,10 +7,8 @@
 #' @return character
 #' @export
 #' @examples
-#' fbind(worlddata$income[c(0.004, 0.434, 1.43)], usadata$spending[c(0.432, 0.546, 0.5433)])
+#' addPercentGDP(worlddata$income[c(0.004, 0.434, 1.43)], usadata$spending[c(0.432, 0.546, 0.5433)])
 #'
-x <- c(0.333, 0.6666, 0.9999, 1.000, 1.33333)
-
 addPercentGDP <- function(x){
   percent <- round(x * 100, digits = 1)
   result <- paste(percent, "% of GDP", sep = "")
